09f868b25153780e027beb79e22b81a33a0ef211
thatchedroof <tnert.wire100@gmail.com>
2024-11-24 18:56:34 -0800

    Fix crate_name

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { initSync, greet } from '../../{{project-name}}/pkg/{{project-name}}';
+	import { initSync, greet } from '../../{{project-name}}/pkg/{{crate_name}}';
 
 	onMount(async () => {
-		const response = await fetch('/{{project-name}}/pkg/{{project-name}}_bg.wasm');
+		const response = await fetch('/{{project-name}}/pkg/{{crate_name}}_bg.wasm');
 		const wasmBinary = await response.arrayBuffer();
 		initSync(wasmBinary);
         greet("{{project-name}}");
